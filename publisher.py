2f94a0c42a02b3cddc4083385bd80074f547df2f
ytani01 <yoichi@tanibayashi.jp>
2018-12-15 03:31:03 +0900

    update

--- publisher.py
+++ publisher.py
@@ -13,12 +13,14 @@ PORT		= 8883
 CACERT		= '/home/pi/mqtt.beebotte.com.pem'
 BME280_ADDR	= 0x76
 
-INTERVAL_SEC	= 300  # sec
+DEF_OUT_FILE	= 'out.csv'
+
+DEF_INTERVAL_SEC= 300  # sec
 
 #####
 def print_usage():
     print()
-    print('usage: {0} token_str channel resource'.format(MYNAME))
+    print('usage: %s token_str channel resource [interval [out_file]]' % (MYNAME))
     print()
 
 #####
@@ -30,13 +32,25 @@ def mqtt_publish(client, topic, data_json):
 
 #####
 def main():
-    if len(sys.argv) != 4:
+    interval_sec = DEF_INTERVAL_SEC
+    out_file = DEF_OUT_FILE
+    
+    if len(sys.argv) < 4 or len(sys.argv) > 6:
         print_usage()
         sys.exit(1)
 
     token_str = sys.argv[1]
     ch_name = sys.argv[2]
     res_name = sys.argv[3]
+    if len(sys.argv) >= 5:
+        interval_sec = int(sys.argv[4])
+    if len(sys.argv) >= 6:
+        out_file = sys.argv[5]
+    print('token_str: %s' % token_str)
+    print('ch_name: %s' % ch_name)
+    print('res_name: %s' % res_name)
+    print('interval_sec: %d' % interval_sec)
+    print('out_file: %s' % out_file)
 
     topic = ch_name + '/' + res_name
     print('topic: ' + topic)
@@ -50,23 +64,37 @@ def main():
 
     bme280 = BME280I2C(BME280_ADDR)
 
+    prev_temp = 0.0
     while True:
         if not bme280.meas():
             print('Error: BME280')
             sys.exit(1)
 
-        data_val['data']['temp']	= int(bme280.T * 100) / 100
-        data_val['data']['humidity']	= int(bme280.H * 100) / 100
-        data_val['data']['pressure']	= int(bme280.P)
-        data_val['ts'] = int(time.time() * 1000)
-        #print(data_val)
-
-        data_json = json.dumps(data_val)
-        print(data_json)
-
-        mqtt_publish(client, topic, data_json)
-            
-        time.sleep(INTERVAL_SEC)
+        print(bme280.T)
         
+        if abs(bme280.T - prev_temp) >= 0.5:
+            prev_temp = bme280.T
+            
+            now_sec = time.time()
+            ts_str = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(now_sec))
+        
+            ## write csv data
+            out_str = '%s, %.1f,C, %.1f,%%, %d,hPa' % (ts_str, bme280.T, bme280.H, bme280.P)
+            print(out_str)
+            with open(out_file, mode='a') as f:
+                f.write(out_str + '\n')
+
+            ## publish json data
+            data_val['data']['temp']	= int(bme280.T * 100) / 100
+            data_val['data']['humidity']	= int(bme280.H * 100) / 100
+            data_val['data']['pressure']	= int(bme280.P)
+            data_val['ts'] = int(now_sec * 1000)
+            #print(data_val)
+            data_json = json.dumps(data_val)
+            print(data_json)
+            mqtt_publish(client, topic, data_json)
+
+        time.sleep(interval_sec)
+            
 if __name__ == '__main__':
     main()
